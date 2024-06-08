ba5ff6874e81c642b4e8241a399c27f17e14d0f6
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2024-06-08 15:55:23 -0300

    Create UsersManager.js

---
+++ CLase4/UsersManager.js
@@ -0,0 +1,30 @@
+
+class UserManager{
+
+    static Usuarios = []
+
+    constructor(){}
+
+    encriptar(password){
+        const crypto = require('crypto')
+        const algoritmo = "aes128"
+        const key = crypto.randomBytes(16)
+        const iv = crypto.randomBytes(16)
+        const cipher = crypto.createCipheriv(algoritmo,key,iv)
+        const passwordEncripted = Buffer.concat([cipher.update(password),cipher.final()])
+        return {
+            iv: iv.toString("hex"),
+            encipted: passwordEncripted.toString("hex")
+        }
+    }
+    crearUsuario(Nombre,Apellido,NombreDeUsuario,password){
+        const psw = this.encriptar(password).encipted
+        const usuario = [Nombre,Apellido,NombreDeUsuario,psw]
+        UserManager.Usuarios.push(usuario)
+    }
+
+}
+
+u = new UserManager()
+u.crearUsuario("pepe","alfonso","pepe2024",'12345')
+console.log(UserManager.Usuarios)
