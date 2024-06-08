43bd75fb4b73dd74d28d21473740794586ecb518
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2024-06-07 21:51:26 -0300

    7-6-2024

---
+++ Clase3/CalculadoraPositiva.js
@@ -0,0 +1,58 @@
+const sumar=(num1,num2)=>{
+    return new Promise((resolve,reject)=>{
+        if ((num1==0)||(num2==0)){
+            reject("Operación innecesaria")
+        }else{
+            if(num1+num2<0) { "La calculadora sólo debe devolver valores positivos"
+            }else resolve(num1+num2)
+        }
+    })
+}
+
+const restar=(num1,num2)=>{
+    return new Promise((resolve,reject)=>{
+        if ((num1==0)||(num2==0)){
+            reject("Operación inválida")
+        }else{
+            if(num1-num2<0) {"La calculadora sólo puede devolver valores positivos"
+            }else resolve(num1-num2)
+        }
+    })
+}
+
+const multiplicar=(num1,num2)=>{
+    return new Promise((resolve,reject)=>{
+        if ((num1<0)||(num2<0)){
+            reject("Operación inválida")
+        }else{
+            if(num1*num2<0) {"La calculadora sólo puede devolver valores positivos"
+            }else resolve(num1*num2)
+        }
+    })
+}
+
+const division=(num1,num2)=>{
+    return new Promise((resolve,reject)=>{
+        if ((num2==0)){
+            reject("Operación inválida")
+        }else{
+            if(num1/num2<0) {"La calculadora sólo puede devolver valores positivos"
+            }else resolve(num1/num2)
+        }
+    })
+}
+
+const calculos = async()=>{
+
+    try {
+       result = await sumar(10,15)
+       console.log(result)
+    } catch (error) {
+        console.log(error)
+    }
+
+
+}
+
+calculos()
+
