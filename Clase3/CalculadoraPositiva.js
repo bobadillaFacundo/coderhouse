b809e1ed9cebf7a4316228aa4d4fb4e7f45b71df
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2024-06-11 19:43:34 -0300

    .

--- Clase3/CalculadoraPositiva.js
+++ Clase3/CalculadoraPositiva.js
@@ -1,52 +1,56 @@
-const sumar=(num1,num2)=>{
-    return new Promise((resolve,reject)=>{
-        if ((num1==0)||(num2==0)){
+const sumar = (num1, num2) => {
+    return new Promise((resolve, reject) => {
+        if ((num1 == 0) || (num2 == 0)) {
             reject("Operación innecesaria")
-        }else{
-            if(num1+num2<0) {reject( "La calculadora sólo debe devolver valores positivos")
-            }else resolve(num1+num2)
+        } else {
+            if (num1 + num2 < 0) {
+                reject("La calculadora sólo debe devolver valores positivos")
+            } else resolve(num1 + num2)
         }
     })
 }
 
-const restar=(num1,num2)=>{
-    return new Promise((resolve,reject)=>{
-        if ((num1==0)||(num2==0)){
+const restar = (num1, num2) => {
+    return new Promise((resolve, reject) => {
+        if ((num1 == 0) || (num2 == 0)) {
             reject("Operación inválida")
-        }else{
-            if(num1-num2<0) {reject("La calculadora sólo puede devolver valores positivos")
-            }else resolve(num1-num2)
+        } else {
+            if (num1 - num2 < 0) {
+                reject("La calculadora sólo puede devolver valores positivos")
+            } else resolve(num1 - num2)
         }
     })
 }
 
-const multiplicar=(num1,num2)=>{
-    return new Promise((resolve,reject)=>{
-        if ((num1<0)||(num2<0)){
+const multiplicar = (num1, num2) => {
+    return new Promise((resolve, reject) => {
+        if ((num1 < 0) || (num2 < 0)) {
             reject("Operación inválida")
-        }else{
-            if(num1*num2<0) {reject("La calculadora sólo puede devolver valores positivos")
-            }else resolve(num1*num2)
+        } else {
+            if (num1 * num2 < 0) {
+                reject("La calculadora sólo puede devolver valores positivos")
+            } else resolve(num1 * num2)
         }
     })
 }
 
-const division=(num1,num2)=>{
-    return new Promise((resolve,reject)=>{
-        if ((num2==0)){
+const division = (num1, num2) => {
+    return new Promise((resolve, reject) => {
+        if ((num2 == 0)) {
             reject("Operación inválida")
-        }else{
-            if(num1/num2<0) {reject("La calculadora sólo puede devolver valores positivos")
-            }else resolve(num1/num2)
+        } else {
+            if (num1 / num2 < 0) {
+                reject("La calculadora sólo puede devolver valores positivos")
+            } else resolve(num1 / num2)
         }
     })
 }
 
-const calculos = async()=>{
+const calculos = async () => {
 
     try {
-       result = await sumar(15,15)
-       console.log(result)
+        result = await sumar(15, 15)
+        console.log(result)
     } catch (error) {
         console.log(error)
     }
--- Clase5/ProductManager.js
+++ Clase5/ProductManager.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
 
-class ProductManager{
+class ProductManager {
 
-    #id 
+    #id
 
-    constructor(path){
-        this.#id=0;
+    constructor(path) {
+        this.#id = 0;
         this.path = path
     }
 
-    getProducts(){
+    getProducts() {
         const productos = this.getProductsFromFile()
         return productos;
     }
 
-    addProduct(title,description,price,thumbnail,code,stock){
+    addProduct(title, description, price, thumbnail, code, stock) {
+        // this.#id=Math.random();
         this.#id++;
         let producto = {
             id: this.#id,
@@ -26,69 +27,68 @@ class ProductManager{
             stock
         }
         const productos = this.getProductsFromFile()
-        const result = this.validarId(this.#id)
-        if (!result) {
+        // const result = this.validarId(this.#id)
+        // if (!result) {
+        //     productos.push(producto)
+        //     this.saveProductsToFile(productos)
+        //     return "Exito"
+        // } else return "codigo ya existe"
         productos.push(producto)
         this.saveProductsToFile(productos)
-        return "Exito"
-        }else return "codigo ya existe"
+
     }
 
-    getProductById(id){
+    getProductById(id) {
         const p = this.validarId(id)
 
         if (!p)
             return `ERROR, no se encuentra ID`
-        else 
+        else
             return p;
     }
 
-    validarId(id){
+    validarId(id) {
         const productos = this.getProductsFromFile()
-        const result = productos.find((producto)=>producto.id===id)
+        const result = productos.find((producto) => producto.id === id)
         return result
     }
 
-    deleteProduct(id){
+    deleteProduct(id) {
         const p = this.validarId(id)
-    
-        if (!p)
-        {
+        if (!p) {
             return `ERROR, no se encuentra ID ${id}`
         }
-        else 
-        {
-            const products = p.filter((product) => product.id!==id)
+        else {
+            const arr = this.getProductsFromFile();
+            const products = arr.filter((product) => product.id !== id)
             this.saveProductsToFile(products)
             return "Exito";
-        }    
+        }
     }
 
     getProductsFromFile() {
         try {
-          const data = fs.readFileSync(this.path, 'utf8')
-          return JSON.parse(data)
+            const data = fs.readFileSync(this.path, 'utf8')
+            return JSON.parse(data)
         } catch (error) {
-          // Si el archivo no existe o está vacío, devuelve un arreglo vacío
-          return []
+            // Si el archivo no existe o está vacío, devuelve un arreglo vacío
+            return []
         }
-      }
+    }
 
     saveProductsToFile(product) {
         fs.writeFileSync(this.path, JSON.stringify(product), 'utf8')
-      }
+    }
 
 }
 
 
 
 const a = new ProductManager("./Clase5")
-a.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
-/*a.addProduct("producto prueba 2","Este es un producto prueba",2003434,"Sin imagen","abc12345",2533);
-console.log(a.getProducts())
-console.log(a.getProductById(2))*/
-console.log(a.getProducts())
-a.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25);
+a.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+a.addProduct("producto prueba2", "Este es un producto prueba2", 200, "Sin imagen", "abc123", 25);
+console.log("Productos:")
 console.log(a.getProducts())
 a.deleteProduct(1)
-
+console.log("Productos:")
+console.log(a.getProducts())
