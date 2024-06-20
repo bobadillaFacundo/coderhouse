c569ab72d5a8f826392bb80f4c62e49bf2fda612
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2024-06-20 20:01:38 -0300

    20/06/2024

---
+++ Clase9/src/routes/views.register.js
@@ -0,0 +1,14 @@
+import ex from "express"
+
+const router = ex.Router()
+
+router.get("/",(req,res)=>{
+    res.render("register")
+})
+
+router.post("/",(req,res)=>{
+    const user = req.body
+    console.log(user);
+})
+
+export default router
