28b7799e47bc061848f7d281aa71149bb0da97a2
bobadillaFacundo <57062710+bobadillaFacundo@users.noreply.github.com>
2024-06-24 21:28:38 -0300

    .

--- Clase11 ACTIVIDAD1/src/public/js/index.js
+++ Clase11 ACTIVIDAD1/src/public/js/index.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
     Swal.fire({
         title: 'Identificarse',
         input: 'text',
-        toast:true,
         text: 'Ingresa el usuario para identificarse en el chat',
         // inputValidador: (value) => {
         //     return !value && 'Necesitas escribir un nombre de usuario para conectarse'
@@ -16,35 +15,35 @@ document.addEventListener('DOMContentLoaded', () => {
         user = result.value
         socket.emit('identificarse', user)
 
-        socket.on('entrada', () => {
-            const mensajeInput = document.getElementById('texto')
-            mensajeInput.value =  `se conecto ${user} `
-        })
-
-        socket.on('mensaje_servidor_todos', (data) => {
-            const mensajeInput = document.getElementById('texto')
-            let result = ''
-            data.forEach(element => {
-                result += `${element.id} dice: ${element.data} ` + "\n"
-            })
-            mensajeInput.value = result
-        }
-        )
-
-        const mensajeInput = document.getElementById('input');
-        const respuestaDiv = document.getElementById('enviar');
-
-        respuestaDiv.addEventListener('click', () => {
-            const mensaje = mensajeInput.value
-            if (mensaje) {
-                // Enviar mensaje al servidor
-                socket.emit('message', mensaje)
-                mensajeInput.value = '' // Limpiar el input
-            }
-        }
-        )
+        
+    })
+    socket.on('entrada', () => {
+        const mensajeInput = document.getElementById('texto')
+        mensajeInput.value =  `se conecto ${user} `
     })
 
+    socket.on('mensaje_servidor_todos', (data) => {
+        const mensajeInput = document.getElementById('texto')
+        let result = ''
+        data.forEach(element => {
+            result += `${element.id} dice: ${element.data} ` + "\n"
+        })
+        mensajeInput.value = result
+    }
+    )
+
+    const mensajeInput = document.getElementById('input');
+    const respuestaDiv = document.getElementById('enviar');
+
+    respuestaDiv.addEventListener('click', () => {
+        const mensaje = mensajeInput.value
+        if (mensaje) {
+            // Enviar mensaje al servidor
+            socket.emit('message', mensaje)
+            mensajeInput.value = '' // Limpiar el input
+        }
+    }
+    )
 
 
 })
